16855507d1ef185fb9a7c9a3d7ac0edc891b1055
ITPLUS Academy <thanghoang064@gmail.com>
2023-01-10 09:07:28 +0700

    update

---
+++ bai3_oop/huongdoituong.php
@@ -0,0 +1,13 @@
+<?php
+// khai báo class
+class SinhVien {
+    //đây là thuộc tính -> hay còn được gọi biến
+    public $tenSV;
+    public $maSV;
+    public $namSinh;
+    //đây là phương thức -> hay còn được gọi là hàm
+    public function hienThiThongTinSV() {
+
+    }
+}
+?>
