c49a34e390a488c240520d98491f88826102d229
ITPLUS Academy <thanghoang064@gmail.com>
2023-01-31 10:16:55 +0700

    update

---
+++ bai3_oop/triutuong.php
@@ -0,0 +1,21 @@
+<?php
+abstract class DongVat {
+    //định nghĩa 1 phương thức trìu tượng (hàm  trìu tượng)
+    abstract function chay();
+}
+class ConCho extends DongVat {
+    public function nhay() {
+
+    }
+    public function chay()
+    {
+       echo "Con chó chạy bằng 4 chân";
+    }
+}
+$conCho = new ConCho();
+$conCho->chay();
+//tìm hiểu xem tại sao phải định nghĩa 1 phương thức abstract và phương thức đó có
+// ý nghĩa gì
+// kiểm tra điều chỉnh lại phiên bản xampp
+// cài đặt composer
+?>
